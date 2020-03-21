16bc0de46f46308a693c5eea831bae1a7995c183
atomink <atomink@gmail.com>
2020-03-21 14:28:15 +0300

    ++ realization of network class

--- network/src/net/protoprint/chat/network/TCPConnection.java
+++ network/src/net/protoprint/chat/network/TCPConnection.java
@@ -10,11 +10,16 @@ public class TCPConnection {
 	private final Socket socket;
 	//поток слушающий входящее соединение
 	private final Thread rxThread;
-
+	private final TCPConnectionListener eventListener;
 	private final BufferedReader in;
 	private final BufferedWriter out;
 
-	public TCPConnection(Socket socket) throws IOException {
+	public TCPConnection(TCPConnectionListener eventListener, String ipAddr, int port) throws IOException {
+		this(eventListener, new Socket(ipAddr, port));
+	}
+
+	public TCPConnection(TCPConnectionListener eventListener, Socket socket) throws IOException {
+		this.eventListener = eventListener;
 		this.socket = socket;
 		in = new BufferedReader(new InputStreamReader(socket.getInputStream(), Charset.forName("UTF-8")));
 		out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), Charset.forName("UTF-8")));
@@ -22,14 +27,43 @@ public class TCPConnection {
 			@Override
 			public void run() {
 				try {
-					String msg = in.readLine();
+					eventListener.onConnectionReady(TCPConnection.this);
+					while (!rxThread.isInterrupted()){
+						eventListener.onReceiveString(TCPConnection.this, in.readLine());
+					}
 				} catch (IOException e) {
-					e.printStackTrace();
+					eventListener.onException(TCPConnection.this, e);
 				} finally {
-
+					eventListener.onDisconnect(TCPConnection.this);
 				}
 			}
 		});
 		rxThread.start();
+	}
+
+	//Делаем потокобезопасный метод для отправки сообщения
+	public synchronized void sendString(String value) {
+		try {
+			out.write(value + "\r\n");
+			out.flush();
+		} catch (IOException e) {
+			eventListener.onException(TCPConnection.this, e);
+			disconnect();
+		}
+	}
+
+	//Делаем потокобезопасный метод для отключения
+	public synchronized void disconnect() {
+		rxThread.interrupt();
+		try {
+			socket.close();
+		} catch (IOException e) {
+			eventListener.onException(TCPConnection.this, e);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "TCPConnection: " + socket.getInetAddress() + ": " + socket.getPort();
 	}
 }
--- server/src/net/protoprint/chat/server/ChatServer.java
+++ server/src/net/protoprint/chat/server/ChatServer.java
@@ -1,9 +1,54 @@
 package net.protoprint.chat.server;
 
-public class ChatServer {
+import net.protoprint.chat.network.TCPConnection;
+import net.protoprint.chat.network.TCPConnectionListener;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
+public class ChatServer implements TCPConnectionListener {
 
 	public static void main(String[] args) {
+		new ChatServer();
+	}
+
+	private final ArrayList<TCPConnection> connections = new ArrayList<>();
+
+	private ChatServer() {
+
+		System.out.println("Server running...");
+
+		try (ServerSocket serverSocket = new ServerSocket(8189)) {
+			while (true) {
+				try {
+					new TCPConnection(this, serverSocket.accept());
+				}catch (IOException e) {
+					System.out.println("TCPconnection exception: " + e);
+				}
+			}
+		}catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void onConnectionReady(TCPConnection tcpConnection) {
 
 	}
 
+	@Override
+	public void onReceiveString(TCPConnection tcpConnection, String value) {
+
+	}
+
+	@Override
+	public void onDisconnect(TCPConnection tcpConnection) {
+
+	}
+
+	@Override
+	public void onException(TCPConnection tcpConnection, Exception e) {
+
+	}
 }
