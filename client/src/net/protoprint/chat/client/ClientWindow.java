47a309832b67f7fff3754f1e381d9ed54249b66d
atomink <atomink@gmail.com>
2020-03-21 13:14:50 +0300

    Initial commit

---
+++ client/src/net/protoprint/chat/client/ClientWindow.java
@@ -0,0 +1,9 @@
+package net.protoprint.chat.client;
+
+public class ClientWindow {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ server/src/net/protoprint/chat/server/ChatServer.java
@@ -0,0 +1,9 @@
+package net.protoprint.chat.server;
+
+public class ChatServer {
+
+	public static void main(String[] args) {
+
+	}
+
+}
